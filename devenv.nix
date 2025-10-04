5162a4595d0c645359770fae1e70b168059e5d51
Ilia Choly <ilia.choly@gmail.com>
2025-10-04 09:23:29 -0400

    Use devenv

---
+++ devenv.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  packages = [
+    pkgs.git
+    pkgs.gh
+    pkgs.ripgrep
+    pkgs.nodejs_20
+  ];
+
+  languages.javascript = {
+    enable = true;
+    package = pkgs.nodejs_20;
+  };
+
+  claude.code.enable = true;
+}
